3287b434e82085479f9c3fb91b70da69236f5d4e
mi-hye <satge13@gmail.com>
2024-06-13 20:57:26 +0900

    feat: test할 트리 추가

---
+++ test.js
@@ -0,0 +1,9 @@
+import Selector from "./Selector.js";
+
+const $btn = Selector.querySelector("button");
+const $secondDiv = Selector.querySelector("#second-div");
+const $li = Selector.querySelector(".li");
+
+console.dir($btn);
+console.dir($secondDiv);
+console.dir($li);
